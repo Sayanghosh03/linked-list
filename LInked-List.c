d7f854900bda22f87ae4761675f37756a1a98f2a
Sayanghosh03 <ghoshsayan993@gmail.com>
2023-07-31 17:53:04 +0530

    Full program of insertion and Display of a linked list

--- LInked-List.c
+++ LInked-List.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // defining a structure with one integer and one address part that is a structure type pointer
 typedef struct node
@@ -7,18 +8,41 @@ typedef struct node
    int data;
    struct node *next;
 } NODE;
-
+// Displaying all nodes
+void display(NODE *p)
+{
+   while (p != NULL)
+   {
+      printf("%d", p->data);
+      p = p->next;
+   }
+   printf("NULL");
+}
 int main()
 {
    NODE *head = NULL, *cur;
+   char ch;
    head = (NODE *)malloc(sizeof(NODE));
    cur = head;
    while (1)
    {
       printf("Enter any data");
       scanf("%d", &cur->data);
-      printf("ener");
+      printf("Continue? (Y/N)");
+      fflush(stdin);
+      scanf("%c", &ch);
+      if (toupper(ch) == 'Y')
+      {
+         cur->next = (NODE *)malloc(sizeof(NODE));
+         cur = cur->next;
+      }
+      else
+      {
+         cur->next = NULL;
+         break;
+      }
    }
+   display(head);
 
    return 0;
 }
